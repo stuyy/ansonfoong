986bc319b51da4beb555de057e28faad5fdbbc30
Stuyy <ansfoong@gmail.com>
2021-10-24 16:09:45 -0400

    Move Header from Home to Global Component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,10 @@
 import { NextPage } from "next";
-import { Header } from "../../components/Header";
 import { HomeSection } from "../../components/HomeComponents/HomeSection";
 
 const HomePage: NextPage = () => {
   return (
     <div className="page">
-      <div className="page-body">
-        <Header />
+      <div className="home-page-body">
         <HomeSection />
       </div>
     </div>
