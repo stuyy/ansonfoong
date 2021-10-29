afeacc08255e210e395f01502bca6326153071dc
Anson <ansfoong@gmail.com>
2021-10-29 02:37:27 -0700

    add responsive styling for home page and page-body class

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,17 +4,19 @@ import styles from "./index.module.scss";
 
 export const Navigation = () => (
   <header className={styles.header}>
-    <div>
-      <h1>ansonfoong</h1>
+    <div className={styles["header-container"]}>
+      <div>
+        <h1>ansonfoong</h1>
+      </div>
+      <ul>
+        {routes.map((route) => (
+          <li key={route.name} className={styles.header_link}>
+            <Link href={route.path}>
+              <a>{route.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-    <ul>
-      {routes.map((route) => (
-        <li key={route.name} className={styles.header_link}>
-          <Link href={route.path}>
-            <a>{route.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
   </header>
 );
